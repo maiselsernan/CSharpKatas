9558feed69eb60d63643f60866cb5073a03e3c82
Ernan <maisels@gmail.com>
2024-03-17 12:42:08 +0200

    DigitalRoot

---
+++ CSharpKatas.Test/DigitalRootTests.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharpKatas.Tests
+{
+    [TestClass()]
+    public class DigitalRootTests
+    {
+        [TestMethod()]
+        public void CalculateDigitalRootTest()
+        {
+            //arrange
+            var digitalRoot = new DigitalRoot();
+            //act
+            var result = digitalRoot.CalculateDigitalRoot(16);
+            //assert
+            Assert.AreEqual(7, result);
+            
+        }
+    }
+}
---
+++ CSharpKatas/DigitalRoot.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpKatas
+{
+    public class DigitalRoot
+    {
+        public  int CalculateDigitalRoot(long n)
+        {
+            long sum = 0;
+            while (n > 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            if (sum >9)
+            {
+                return CalculateDigitalRoot(sum);
+            }
+            return (int) sum;
+                       
+        }
+    }
+}
