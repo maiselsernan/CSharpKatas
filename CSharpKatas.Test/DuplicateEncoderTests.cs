776c56aa062086fc506462ebbdf5f5a26d76fe1c
Ernan <maisels@gmail.com>
2024-03-19 13:10:05 +0200

    DuplicateEncoder

---
+++ CSharpKatas.Test/DuplicateEncoderTests.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharpKatas.Tests
+{
+    [TestClass()]
+    public class DuplicateEncoderTests
+    {
+        [TestMethod()]
+        public void DuplicateEncodeTest()
+        {
+            Assert.AreEqual("(((", DuplicateEncoder.DuplicateEncode("din"));
+            Assert.AreEqual("()()()", DuplicateEncoder.DuplicateEncode("recede"));
+            Assert.AreEqual(")())())", DuplicateEncoder.DuplicateEncode("Success"), "should ignore case");
+            Assert.AreEqual("))((", DuplicateEncoder.DuplicateEncode("(( @"));
+        }
+    }
+}
---
+++ CSharpKatas/DuplicateEncoder.cs
@@ -0,0 +1,26 @@
+﻿//https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/csharp
+using System.Text;
+
+namespace CSharpKatas
+{
+    public class DuplicateEncoder
+    {
+        public static string DuplicateEncode(string word)
+        {
+            StringBuilder temp = new StringBuilder();
+            foreach (var c in word.ToLower())
+            {
+                if (word.Count(x=> char.ToLower(x) == char.ToLower(c)) > 1)
+                {
+                    temp.Append(')');
+                }
+                else
+                {
+                    temp.Append('(');
+                }
+            }
+            word = temp.ToString();
+            return word;
+        }
+    }
+}
